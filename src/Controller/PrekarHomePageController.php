28716cb3eb733fa2d8ce4290a8185c03941cc266
0tomai <lounis.timon.666@gmail.com>
2025-01-16 10:45:22 +0100

    HomePage

---
+++ src/Controller/PrekarHomePageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PrekarHomePageController extends AbstractController
+{
+    #[Route('', name: 'app_prekar_home_page')]
+    public function index(): Response
+    {
+        return $this->render('prekar_home_page/index.html.twig', [
+            'controller_name' => 'PrekarHomePageController',
+        ]);
+    }
+}
