77b17fcdc32b9691536849bfd4dbfb979b1b4fc8
Abrlh <serabralah@gmail.com>
2025-01-27 22:47:35 +0100

    blank user profile

---
+++ src/Controller/UserProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserProfileController extends AbstractController
+{
+    #[Route('/user/profile', name: 'app_user_profile')]
+    public function index(): Response
+    {
+        if(!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('user_profile/profile.html.twig', [
+            'controller_name' => 'UserProfileController',
+        ]);
+    }
+}
