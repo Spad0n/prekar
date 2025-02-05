5882b83987f0635635496305c1ce53de81c99da9
noyorez <ivan.osadtsiv1@gmail.com>
2025-02-05 23:01:37 +0100

    choosing existing car for offer

--- src/Controller/Form/OfferController.php
+++ src/Controller/Form/OfferController.php
@@ -22,12 +22,22 @@ final class OfferController extends AbstractController
         }
         $offer = new Offer();
         $offer->setUserOwner($this->getUser()); // auto assign logged-in user
-        $form = $this->createForm(OfferFormType::class, $offer);
+        $userCars = $entityManager->getRepository(Car::class)->findBy(['userOwner' => $this->getUser()]);
+        $form = $this->createForm(OfferFormType::class, $offer, ['user_cars' => $userCars]);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $car = $form->get('car')->getData();
-            $offer->setCar($car);
-            $car->setUserOwner($this->getUser());
+            $existingCar = $form->get('existingCar')->getData();
+            $newCar = $form->get('newCar')->getData();
+            if ($existingCar) {
+                $offer->setCar($existingCar);
+            } elseif ($newCar) {
+                $newCar->setUserOwner($this->getUser());
+                $entityManager->persist($newCar);
+                $offer->setCar($newCar);
+            } else {
+                $this->addFlash('error', 'Please select or create a car.');
+                return $this->redirectToRoute('offer_new');
+            }
             $entityManager->persist($offer);
             $entityManager->flush();
             return $this->redirectToRoute('offer_list');
@@ -38,7 +48,7 @@ final class OfferController extends AbstractController
     }
 
     #[Route('/offer/list', name: 'offer_list')]
-    public function list(EntityManagerInterface $entityManager) { // show existing offers, TODO delete after tests
+    public function list(EntityManagerInterface $entityManager) {
         $offers = $entityManager->getRepository(Offer::class)->findAll();
         return $this->render('offer/list.html.twig', ['offers' => $offers,]);
     }
