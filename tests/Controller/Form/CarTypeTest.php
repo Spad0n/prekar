d28c646372eed2011dd64411880c71bed324ceda
Abrlh <serabralah@gmail.com>
2025-02-15 17:01:04 +0100

    Test for Car

--- tests/Controller/Form/CarTypeTest.php
+++ tests/Controller/Form/CarTypeTest.php
@@ -4,16 +4,23 @@ namespace App\Tests\Form;
 
 use App\Entity\Car;
 use App\Form\CarType;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Validator\Validation;
 
 class CarTypeTest extends TypeTestCase
 {
+    protected function getExtensions()
+    {
+        return [new ValidatorExtension(Validation::createValidator())];
+    }
+
     public function testSubmitValidData()
     {
         $formData = [
             'brand' => 'twingo velault',
             'model' => '1',
-            'registration' => 'AAA-111-WW',
+            'registration' => '123 AB 75',
             'nbSeat' => 5,
             'bootCapacity' => 500,
             'fuelType' => 'petrol',
@@ -30,7 +37,7 @@ class CarTypeTest extends TypeTestCase
         $this->assertInstanceOf(Car::class, $car);
         $this->assertEquals('twingo velault', $car->getBrand());
         $this->assertEquals('1', $car->getModel());
-        $this->assertEquals('AAA-111-WW', $car->getRegistration());
+        $this->assertEquals('123 AB 75', $car->getRegistration());
         $this->assertEquals(5, $car->getNbSeat());
         $this->assertEquals(500, $car->getBootCapacity());
         $this->assertEquals('petrol', $car->getFuelType());
@@ -43,7 +50,7 @@ class CarTypeTest extends TypeTestCase
         $formData = [
             'brand' => 'Velault',
             'model' => 'Clio de fou',
-            'registration' => 'AAA-WWW-ZZZ',
+            'registration' => '123 AB 75',
             'nbSeat' => 4,
             'fuelType' => 'petrol',
         ];
@@ -56,4 +63,24 @@ class CarTypeTest extends TypeTestCase
         $car = $form->getData();
         $this->assertNull($car->getBootCapacity());
     }
+
+    public function testSubmitFail()
+    {
+        $formData = [
+            'brand' => 'twingo velault',
+            'model' => '1',
+            'registration' => '12253 zaAB 75',
+            'nbSeat' => 5,
+            'bootCapacity' => 500,
+            'fuelType' => 'petrol',
+        ];
+
+        $form = $this->factory->create(CarType::class);
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSubmitted());
+        $this->assertFalse($form->isValid());
+
+    }
+
 }
