06cee9f2bcbab6c408d5a19a4d67a227fe4c33b9
0tomai <lounis.timon.666@gmail.com>
2025-03-27 08:54:55 +0100

    we can now see the driving licence number

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -632,7 +632,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $driverLicense = null;
 
-    public function getDriverLicenceNb(): ?string
+    public function getDriverLicense(): ?string
     {
         return $this->driverLicense;
     }
